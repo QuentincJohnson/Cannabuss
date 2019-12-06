f58b98e09e0173bd072883e0168a7b2e65f76d84
QuentincJohnson <quentincjohnson@gmail.com>
2019-12-06 05:40:43 -0500

    started product invintory

--- js/index.js
+++ js/index.js
@@ -1,1 +1,234 @@
-let 
+//PTODUCTS
+
+let products =  [19]
+products[0] = {
+    id:1,
+    name: "Gorilla Glue",
+    image: ,
+    discription:"Original Glue (GG4), developed by GG Strains, is a potent hybrid strain that delivers heavy-handed euphoria and relaxation, leaving you feeling “glued” to the couch. Its chunky, resin-covered buds fill the room with pungent earthy and sour aromas inherited from its parent strains, Chem’s Sister, Sour Dubb, and Chocolate Diesel",
+    price: [9.00,30.00,70],
+    quality: "A+",
+    strain: "Indica",
+    category: "Flower",
+    science: ["11", "4"] //thc -> cbd
+}
+products[1] = {
+    id:2,
+    name: "Zkittlez",
+    image:,
+    discription: "Zkittlez is an indica-dominant mix of Grape Ape and Grapefruit that is crossed with another undisclosed strain to produce this candy-flavored cannabis bred by 3rd Gen Family and Terp Hogz. This award-winning combination took 1st Place at the 2016 Emerald Cup and 1st Place Indica at the 2015 Cannabis Cups in San Francisco and in Michigan" ,
+    price: [9.00,30.00,70],
+    quality: "A+",
+    strain:"Hybrid",
+    category: "Flower",
+    science: ["10", "2"] //thc -> cbd
+}
+products[2] = {
+    id:3,
+    name: "Jack Herer",
+    image: ,
+    discription: "Jack Herer is a sativa-dominant cannabis strain that has gained as much renown as its namesake, the marijuana activist and author of The Emperor Wears No Clothes. Combining a Haze hybrid with a Northern Lights #5 and Shiva Skunk cross, Sensi Seeds created Jack Herer hoping to capture both cerebral elevation and heavy resin production. Its rich genetic background gives rise to several different variations of Jack Herer",
+    price: [9.00,30.00,70],
+    quality: "A+",
+    strain: "Sativa",
+    category: "Flower",
+    science: ["11", "1"] //thc -> cbd
+}
+products[3] = {
+    id:4,
+    name: "Blueberry",
+    image: ,
+    discription: "A true A-List cannabis strain, Blueberry’s legendary status soared to new heights after claiming the High Times’ Cannabis Cup 2000 for Best Indica. The long history of the strain goes back to the late 1970s when American breeder DJ Short was working with a variety of exotic landrace strains.",
+    price: [9.00,30.00,70],
+    quality: "A+",
+    strain:"Indica",
+    category: "Flower",
+    science: ["8", "4"] //thc -> cbd
+}
+products[4] = {
+    id:5,
+    name:"Blue Dream",
+    image: ,
+    discription:"Blue Dream, a sativa-dominant hybrid originating in California, has achieved legendary status among West Coast strains. Crossing Blueberry with Haze, Blue Dream balances full-body relaxation with gentle cerebral invigoration. Novice and veteran consumers alike enjoy the level effects of Blue Dream, which ease you gently into a calm euphoria.",
+    price: [9.00,30.00,70],
+    quality: "A+",
+    strain:"Hybrid",
+    category: "Flower",
+    science: ["11", "0"] //thc -> cbd
+}
+products[5] = {
+    id:6,
+    name:"Acaupulco Glod",
+    image:,
+    discription:"One of the most well-known strains, Acapulco Gold has been likened to dinner at a five-star restaurant. This strain comes from the area in and around Acapulco, Mexico, and its orange hairs resemble a gold nugget, with gold, green, and brown colors and plenty of resin on the buds. An aroma of burnt toffee lingers when the bud is broken up." ,
+    price: [10.00, 80.00, 140.00],
+    quality: "A++",
+    strain: "Sativa",
+    category: "Flower",
+    science: ["", ""] //thc -> cbd
+}
+products[6] = {
+    id:1,
+    name: ,
+    image: ,
+    discription: ,
+    price: [10.00, 80.00, 140.00],
+    quality: ,
+    strain:"A++",
+    category: "Flower",
+    science: ["", ""] //thc -> cbd
+}
+products[7] = {
+    id:1,
+    name: ,
+    image: ,
+    discription: ,
+    price: [10.00, 80.00, 140.00],
+    quality: ,
+    strain: "A++",
+    category: "Flower",
+    science: ["", ""] //thc -> cbd
+}
+products[8] = {
+    id:1,
+    name: ,
+    image: ,
+    discription: ,
+    price: [10.00, 80.00, 140.00],
+    quality: ,
+    strain:"A++",
+    category: "Flower",
+    science: ["", ""] //thc -> cbd
+}
+products[9] = {
+    id:1,
+    name: ,
+    image: ,
+    discription: ,
+    price: [10.00, 80.00, 140.00],
+    quality: ,
+    strain: ,
+    category:"Flower",
+    science: ["", ""] //thc -> cbd
+}
+products[10] = {
+    id:1,
+    name: ,
+    image: ,
+    discription: ,
+    price: [10.00, 80.00, 140.00],
+    quality: ,
+    strain: ,
+    category: "Flower",
+    science: ["", ""] //thc -> cbd
+}
+products[11] = {
+    id:1,
+    name: ,
+    image: ,
+    discription: ,
+    price: [10.00, 80.00, 140.00],
+    quality: ,
+    strain: ,
+    category:"Flower",
+    science: ["", ""] //thc -> cbd
+}
+products[12] = {
+    id:1,
+    name: ,
+    image: ,
+    discription: ,
+    price: [10.00, 80.00, 140.00],
+    quality: ,
+    strain: ,
+    category: "Flower",
+    science: ["", ""] //thc -> cbd
+}
+products[13] = {
+    id:1,
+    name: ,
+    image: ,
+    discription: ,
+    price: [10.00, 80.00, 140.00],
+    quality: ,
+    strain: ,
+    category: "Flower",
+    science: ["", ""] //thc -> cbd
+}
+products[14] = {
+    id:1,
+    name: ,
+    image: ,
+    discription: ,
+    price: [10.00, 80.00, 140.00],
+    quality: ,
+    strain: ,
+    category: "Flower",
+    science: ["", ""] //thc -> cbd
+}
+products[15] = {
+    id:1,
+    name: ,
+    image: ,
+    discription: ,
+    price: [10.00, 80.00, 140.00],
+    quality: ,
+    strain: ,
+    category: "Flower",
+    science: ["", ""] //thc -> cbd
+}
+products[16] = {
+    id:1,
+    name: ,
+    image: ,
+    discription: ,
+    price: [10.00, 80.00, 140.00],
+    quality: ,
+    strain: ,
+    category: ,
+    science: ["", ""] //thc -> cbd
+}
+products[17] = {
+    id:1,
+    name: ,
+    image: ,
+    discription: ,
+    price: [10.00, 80.00, 140.00],
+    quality: ,
+    strain: ,
+    category: ,
+    science: ["", ""] //thc -> cbd
+}
+products[18] = {
+    id:1,
+    name: ,
+    image: ,
+    discription: ,
+    price: [10.00, 80.00, 140.00],
+    quality: ,
+    strain: ,
+    category: ,
+    science: ["", ""] //thc -> cbd
+}
+products[19] = {
+    id:1,
+    name: ,
+    image: ,
+    discription: ,
+    price: [10.00, 80.00, 140.00],
+    quality: ,
+    strain: ,
+    category: ,
+    science: ["", ""] //thc -> cbd
+}
+products[20] = {
+    id:1,
+    name: ,
+    image: ,
+    discription: ,
+    price: [10.00, 80.00, 140.00],
+    quality: ,
+    strain: ,
+    category: ,
+    science: ["", ""] //thc -> cbd
+}
