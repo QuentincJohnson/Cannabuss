0033a0faa5d6bca075e16cd9142abe5d9d982e5c
QuentincJohnson <quentincjohnson@gmail.com>
2019-12-10 10:08:06 -0500

    fixed imgs

--- js/index.js
+++ js/index.js
@@ -26,7 +26,7 @@ products[1] = {
 products[2] = {
     id:3,
     name: "Jack Herer",
-    image: "/img/3.png",
+    image: "img/3.png",
     discription: "Jack Herer is a sativa-dominant cannabis strain that has gained as much renown as its namesake, the marijuana activist and author of The Emperor Wears No Clothes. Combining a Haze hybrid with a Northern Lights #5 and Shiva Skunk cross, Sensi Seeds created Jack Herer hoping to capture both cerebral elevation and heavy resin production. Its rich genetic background gives rise to several different variations of Jack Herer",
     price: [9.00.toFixed(2),30.00.toFixed(2),70.00.toFixed(2)],
     quality: "A+",
@@ -37,7 +37,7 @@ products[2] = {
 products[3] = {
     id:4,
     name: "Blueberry",
-    image: "/img/4.png",
+    image: "img/4.png",
     discription: "A true A-List cannabis strain, Blueberry’s legendary status soared to new heights after claiming the High Times’ Cannabis Cup 2000 for Best Indica. The long history of the strain goes back to the late 1970s when American breeder DJ Short was working with a variety of exotic landrace strains.",
     price: [9.00.toFixed(2),30.00.toFixed(2),70.00.toFixed(2)],
     quality: "A+",
@@ -48,7 +48,7 @@ products[3] = {
 products[4] = {
     id:5,
     name:"Blue Dream",
-    image: "/img/5.png",
+    image: "img/5.png",
     discription:"Blue Dream, a sativa-dominant hybrid originating in California, has achieved legendary status among West Coast strains. Crossing Blueberry with Haze, Blue Dream balances full-body relaxation with gentle cerebral invigoration. Novice and veteran consumers alike enjoy the level effects of Blue Dream, which ease you gently into a calm euphoria.",
     price: [9.00.toFixed(2),30.00.toFixed(2),70.00.toFixed(2)],
     quality: "A+",
@@ -59,7 +59,7 @@ products[4] = {
 products[5] = {
     id:6,
     name:"Acaupulco Glod",
-    image:"/img/6.png",
+    image:"img/6.png",
     discription:"One of the most well-known strains, Acapulco Gold has been likened to dinner at a five-star restaurant. This strain comes from the area in and around Acapulco, Mexico, and its orange hairs resemble a gold nugget, with gold, green, and brown colors and plenty of resin on the buds. An aroma of burnt toffee lingers when the bud is broken up." ,
     price: [10.00.toFixed(2), 80.00.toFixed(2), 140.00.toFixed(2)],
     quality: "A++",
@@ -70,7 +70,7 @@ products[5] = {
 products[6] = {
     id:7,
     name: "Grape Ape",
-    image: "/img/7.png",
+    image: "img/7.png",
     discription: "Grape Ape, propagated by Apothecary Genetics and Barney’s Farm, is a mostly indica strain that crosses Mendocino Purps, Skunk, and Afghani. Named for its distinct grape-like smell, this indica is known for providing carefree relaxation that can help soothe pain, stress, and anxiety.",
     price: [10.00.toFixed(2), 80.00.toFixed(2), 140.00.toFixed(2)],
     quality:"A++",
@@ -81,7 +81,7 @@ products[6] = {
 products[7] = {
     id:8,
     name: "Gelato",
-    image: "/img/8.png",
+    image: "img/8.png",
     discription: "Gelato (also referred to as \"Larry Bird\") is a tantalizing hybrid cannabis strain from Cookie Fam and Sherbinski, following in the footsteps of its parents Sunset Sherbet and Thin Mint GSC. This Bay Area, California native gets its name from the fruity, dessert-like aroma, a common genetic thread among the Cookie family." ,
     price: [10.00.toFixed(2), 80.00.toFixed(2), 140.00.toFixed(2)],
     quality:"A++",
@@ -92,7 +92,7 @@ products[7] = {
 products[8] = {
     id:9,
     name: "Maui Wowie",
-    image: "/img/9.png",
+    image: "img/9.png",
     discription: "Maui Wowie (not Maui Waui) is a classic sativa whose tropical flavors and stress-relieving qualities will float you straight to the shores of Hawaii where this strain originally comes from. Since its beginnings in the island’s volcanic soil, Maui Wowie has spread across the world to bless us with its sweet pineapple flavors and high-energy euphoria.",
     price: [10.00.toFixed(2), 80.00.toFixed(2), 140.00.toFixed(2)],
     quality:"A++",
@@ -103,7 +103,7 @@ products[8] = {
 products[9] = {
     id:10,
     name:"Nuken",
-    image: "/img/10.png",
+    image: "img/10.png",
     discription: "The mostly indica Nuken is a Canadian strain bred by combining genetics from Kish (a cross of two Shishkaberry parents) and God Bud. Her even-keeled effects are delivered alongside a sweet, earthy aroma of fresh herbs and grass. While undeniably strong, Nuken typically leaves you functional enough to still enjoy hobbies and the company of friends.",
     price: [11.00.toFixed(2), 85.00.toFixed(2), 150.00.toFixed(2)],
     quality:"A+++",
@@ -114,7 +114,7 @@ products[9] = {
 products[10] = {
     id:11,
     name: "AK-47",
-    image: "/img/11.png",
+    image: "img/11.png",
     discription: "Don't let its intense name fool you: AK-47 will leave you relaxed and mellow. This sativa-dominant strain mixes Colombian, Mexican, Thai, and Afghani varieties, bringing together a complex blend of flavors and effects. AK-47 may deliver a steady and long-lasting cerebral buzz that will keep you mentally alert and engaged in creative or social activities.",
     price: [11.00.toFixed(2), 85.00.toFixed(2), 150.00.toFixed(2)],
     quality: "A+++",
@@ -125,7 +125,7 @@ products[10] = {
 products[11] = {
     id:12,
     name: "Amnesia",
-    image: "/img/12.png",
+    image: "img/12.png",
     discription:"Amnesia is typically a sativa-dominant cannabis strain with some variation between breeders. Skunk, Cinderella 99, and Jack Herer are some of Amnesia’s genetic forerunners, passing on uplifting, creative, and euphoric effects ideal for treating mood disorders. Growers should expect a 9-10 week flowering period with moderate yields.",
     price: [11.00.toFixed(2), 85.00.toFixed(2), 150.00.toFixed(2)],
     quality: "A+++",
@@ -136,7 +136,7 @@ products[11] = {
 products[12] = {
     id:13,
     name: "3X Crazy",
-    image: "/img/13.png",
+    image: "img/13.png",
     discription: "Also known as Optimus Prime, the indica-dominant 3X Crazy is a three-way cross between OG Kush, Bubba Kush, and Granddaddy Purple. Its aroma is comprised of sweet grape, spicy pepper, and earthy notes passed on by its Bubba Kush parent. This indica can cover the consumer in soothing full-body effects, but not before a brief phase of clear-headed cerebral sizzle.",
     price: [11.00.toFixed(2), 85.00.toFixed(2), 150.00.toFixed(2)],
     quality: "A+++",
@@ -147,7 +147,7 @@ products[12] = {
 products[13] = {
     id:14,
     name: "3 Kings",
-    image: "/img/14.png",
+    image: "img/14.png",
     discription: "The 3 Kings marijuana strain, a holy trinity of Headband, Sour Diesel, and OG Kush, is a sativa-dominant hybrid and genetic masterpiece. Sour tanginess reek from the sage green buds, and its frosty coat gives you a fair warning of the 3 Kings’ potency.",
     price: [12.00.toFixed(2), 90.00.toFixed(2), 160.00.toFixed(2)],
     quality:"Quad",
@@ -158,7 +158,7 @@ products[13] = {
 products[14] = {
     id:15,
     name: "Royal Haze",
-    image: "/img/15.png",
+    image: "img/15.png",
     discription: "Royal Haze (or Royale Haze) by Dinafem Seeds is a mostly sativa strain bred from Skunk, Haze, and Northern Lights genetics. Taking after her Haze parent, this sativa inherits a spicy citrus flavor and buzzing, energetic effects that keep you alert and productive throughout the day.",
     price: [13.00.toFixed(2), 100.00.toFixed(2), 180.00.toFixed(2)],
     quality: "Quad",
@@ -169,7 +169,7 @@ products[14] = {
 products[15] = {
     id:16,
     name: "Afghani",
-    image: "/img/16.png",
+    image: "img/16.png",
     discription: "Afghani is a heavy indica strain named after its geographic origin, where the earliest varieties of cannabis are believed to have grown. Breeders worldwide have come to treasure Afghani for its heavy resin production which is passed on genetically. With a sweet, earthy aroma, Afghani delivers a deep, sedating relaxation and euphoria. ",
     price: [12.00.toFixed(2), 95.00.toFixed(2), 170.00.toFixed(2)],
     quality:"Quad", 
@@ -180,7 +180,7 @@ products[15] = {
 products[16] = {
     id:17,
     name: "white Rhino",
-    image: "/img/shat1.png",
+    image: "img/shat1.png",
     discription: "White Rhino is a hybrid of White Widow and an unknown North American indica strain, resulting a bushy and stout plant. This strain is said to descend from cultivars sourced from Afghanistan, Brazil, and India. White Rhino's high THC content makes it a popular choice among consumers looking for heavy-handed relaxation and symptom relief.",
     price: [65.00.toFixed(2), .175.toFixed(2), 360.00.toFixed(2)],
     quality: "A+++",
@@ -191,7 +191,7 @@ products[16] = {
 products[17] = {
     id:18,
     name: "Clementine",
-    image: "/img/shat2.png",
+    image: "img/shat2.png",
     discription: "Clementine is a sweet, citrus flavored sativa hybrid that combines Tangie with Lemon Skunk genetics. The concentrated live resin of Clementine from Crocket Family Farms took home 2nd Place for the Best Sativa Concentrate at the 2015 Cannabis Cup in Michigan.",
     price: [65.00.toFixed(2), 175.00.toFixed(2), 360.00.toFixed(2)],
     quality:"A+++",
@@ -202,7 +202,7 @@ products[17] = {
 products[18] = {
     id:19,
     name: "SFV OG",
-    image: "/img/shat3.png",
+    image: "img/shat3.png",
     discription:"SFV OG by Cali Connection is a sativa-dominant hybrid that is great for patients who need strong pain relief but don’t want to be stuck on the couch. As the name indicates, this OG Kush relative originates from California’s San Fernando Valley. Although their names are barely distinguishable, SFV OG Kush is actually the Afghani-crossed child to SFV OG. ",
     price: [65.00.toFixed(2), 175.00.toFixed(2), 360.00.toFixed(2)],
     quality: "Quad",
@@ -213,7 +213,7 @@ products[18] = {
 products[19] = {
     id:20,
     name:"Big Smooth",
-    image: "/img/shat4.png",
+    image: "img/shat4.png",
     discription:"Big Smooth by Exotic Genetix is a flavorful cross of OG Blueberry and Cookies and Cream. This indica-dominant hybrid offers a delicious terpene profile that smells sweet and doughy, like blueberry pancakes. Big Smooth grows bushy, medium-tall plants and has a flowering time of 55 to 66 days. Enjoy Big Smooth for its delicious flavor and euphoric, middleweight sedation.",
     price: [65.00.toFixed(2), 175.00.toFixed(2), 360.00.toFixed(2)],
     quality: "Quad",
